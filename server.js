02cc8994b4fa73b312327c3f5d36d840da5560f9
Gabriel <gomesg827@gmail.com>
2025-07-23 10:54:22 -0300

    Teste de respoosta JSON e configurações

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Hello, API!");
+  res.json([{ oi: "oi" }]);
 });
 
 app.listen(PORT, () => {
