2ee512f5a5826fa993c22d2f86cce09334e354eb
Gabriel <gomesg827@gmail.com>
2025-07-23 18:04:24 -0300

    Rota para o GET /agentes/:id pronta.

--- controllers/agentesController.js
+++ controllers/agentesController.js
@@ -4,3 +4,21 @@ import * as agentesRepository from "../repositories/agentesRepository.js";
 export function getAgentes(req, res) {
   res.status(200).json(agentesRepository.obterTodosAgentes());
 }
+
+// GET /agentes/:id
+export function getAgentesId(req, res) {
+  const { id } = req.params;
+  const agente_encontrado = agentesRepository.obterUmAgente(id);
+
+  if (!agente_encontrado) {
+    res.status(404).json({
+      status: 404,
+      message: "ID inexistente",
+      errors: {
+        id: `O usuário com o ID '${id}' não foi encontrador no sistema.`,
+      },
+    });
+  } else {
+    res.status(200).json(agente_encontrado);
+  }
+}
--- routes/agentesRoutes.js
+++ routes/agentesRoutes.js
@@ -5,4 +5,6 @@ const router = express.Router();
 
 router.get("/agentes", agentesController.getAgentes);
 
+router.get("/agentes/:id", agentesController.getAgentesId);
+
 export default router;
