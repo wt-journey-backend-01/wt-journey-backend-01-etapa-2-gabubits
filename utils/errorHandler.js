af0f7d4e105c69cfaa585ba938e2f0d86cbe50c6
Gabriel <gomesg827@gmail.com>
2025-07-25 18:16:22 -0300

    Implementados erros principais da aplicação.

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,8 +1,28 @@
-export class APIError extends Error {
+class APIError extends Error {
   constructor(status, message, errors) {
     super(status, message, errors);
+
+    this.json = { status, message, errors };
     this.status = status;
     this.message = message;
     this.errors = errors;
+  }
+}
+
+export class InvalidIdError extends APIError {
+  constructor(errors) {
+    super(404, "ID inválido", errors);
+  }
+}
+
+export class IdNotFoundError extends APIError {
+  constructor(errors) {
+    super(404, "ID inexistente", errors);
+  }
+}
+
+export class InvalidFormatError extends APIError {
+  constructor(errors) {
+    super(400, "Atributos inválidos", errors);
   }
 }
