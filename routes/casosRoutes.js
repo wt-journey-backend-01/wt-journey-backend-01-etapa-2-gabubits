40dfc5edde3f229d613ec684a990490875f54548
Gabriel <gomesg827@gmail.com>
2025-07-27 12:12:12 -0300

    Routes do caso prontas.

--- routes/casosRoutes.js
+++ routes/casosRoutes.js
@@ -1,5 +1,22 @@
 import express from "express";
+import * as casosController from "../controllers/casosController.js";
 
-const casosRoutes = express.Router();
+const router = express.Router();
 
-export default casosRoutes;
+router.get("/casos", casosController.obterCasos);
+
+router.get("/casos/:id", casosController.obterUmCaso);
+
+router.post("/casos", casosController.criarCaso);
+
+router.put("/casos/:id", casosController.atualizarCaso);
+
+router.patch("/casos/:id", casosController.atualizarCaso);
+
+router.delete("/casos/:id", casosController.apagarCaso);
+
+router.get("/casos/:caso_id/agente", casosController.obterAgenteDoCaso);
+
+router.get("/casos/search", casosController.pesquisarCasos);
+
+export default router;
