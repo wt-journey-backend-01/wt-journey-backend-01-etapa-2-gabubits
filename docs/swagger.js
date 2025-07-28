d8a985dbcb3259e9163092de810fa6fd5bb3b472
Gabriel <gomesg827@gmail.com>
2025-07-28 19:45:56 -0300

    Primeira versão do Swagger implementada.

--- docs/swagger.js
+++ docs/swagger.js
@@ -42,19 +42,32 @@ export const option = {
           summary: "Obtenha todos os agentes",
           description:
             "Obtenha todos os agentes cadastrados no departamento de polícia.",
+          parameters: [
+            {
+              name: "cargo",
+              in: "query",
+              description: "Obter os agentes com o cargo especificado.",
+              required: false,
+              schema: {
+                type: "string",
+              },
+            },
+            {
+              name: "status",
+              in: "query",
+              description:
+                "Obter os agentes ordenados por data de incorporação. Pode ser na ordem ascendente ou descendente.",
+              required: false,
+              schema: {
+                type: "string",
+                default: "dataDeIncorporacao",
+                enum: ["dataDeIncorporacao", "-dataDeIncorporacao"],
+              },
+            },
+          ],
           responses: {
             200: {
-              description: "Operação bem-sucedida",
-              content: {
-                "application/json": {
-                  schema: {
-                    type: "array",
-                    items: {
-                      $ref: "#/components/schemas/Agente",
-                    },
-                  },
-                },
-              },
+              $ref: "#/components/responses/AgenteArray",
             },
           },
         },
@@ -74,20 +87,8 @@ export const option = {
             required: true,
           },
           responses: {
-            200: {
-              description: "Operação bem-sucedida - Agente cadastrado",
-              content: {
-                "application/json": {
-                  schema: {
-                    $ref: "#/components/schemas/Agente",
-                  },
-                  examples: {
-                    Agente: {
-                      $ref: "#/components/examples/ExempleAgente",
-                    },
-                  },
-                },
-              },
+            201: {
+              $ref: "#/components/responses/Agente",
             },
             400: {
               $ref: "#/components/responses/MalformedData",
@@ -100,38 +101,265 @@ export const option = {
           tags: ["agentes"],
           summary: "Obtenha informações do agente pelo ID",
           description:
-            "Retorna todas as informações referentes ao agente especificado pelo ID",
+            "Retorna todas as informações referentes ao agente especificado pelo ID.",
           produces: "application/json",
+          parameters: [{ $ref: "#/components/parameters/idParam" }],
+          responses: {
+            200: {
+              $ref: "#/components/responses/Agente",
+            },
+          },
+        },
+        put: {
+          tags: ["agentes"],
+          summary: "Atualize todas as informações do agente",
+          description:
+            "Todos os parâmetros devem ser passados no corpo de requisição.",
+          parameters: [{ $ref: "#/components/parameters/idParam" }],
+          requestBody: {
+            description: "Atualiza informações do agente",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/idless/Agente",
+                },
+              },
+            },
+          },
+          responses: {
+            200: {
+              $ref: "#/components/responses/Agente",
+            },
+            400: {
+              $ref: "#/components/responses/MalformedData",
+            },
+            404: {
+              $ref: "#/components/responses/InvalidId",
+            },
+          },
+        },
+        patch: {
+          tags: ["agentes"],
+          summary: "Atualize algumas informações do agente",
+          description:
+            "Deve ser passado no mínimo um atributo no corpo de requisição.",
+          parameters: [{ $ref: "#/components/parameters/idParam" }],
+          requestBody: {
+            description: "Atualiza informações do agente",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/idless/Agente",
+                },
+              },
+            },
+          },
+          responses: {
+            200: {
+              $ref: "#/components/responses/Agente",
+            },
+            400: {
+              $ref: "#/components/responses/MalformedData",
+            },
+            404: {
+              $ref: "#/components/responses/InvalidId",
+            },
+          },
+        },
+        delete: {
+          tags: ["agentes"],
+          summary: "Apague um agente",
+          description: "Apague um agente e todos os casos relacionados a ele.",
+          parameters: [{ $ref: "#/components/parameters/idParam" }],
+          responses: {
+            204: {
+              description: "O agente e seus casos foram apagados",
+            },
+          },
+        },
+      },
+      "/casos": {
+        get: {
+          tags: ["casos"],
+          summary: "Obtenha todos os casos",
+          description:
+            "Obtenha todos os casos cadastrados no departamento de polícia.",
           parameters: [
             {
-              name: "id",
-              in: "path",
-              description: "ID do agente",
-              required: true,
+              name: "agente_id",
+              in: "query",
+              description:
+                "Obter todos os casos atribuídos à um agente específico.",
+              required: false,
               schema: {
                 type: "string",
                 format: "uuid",
               },
             },
+            {
+              name: "status",
+              in: "query",
+              description: "Obter todos os casos com o status especificado.",
+              required: false,
+              schema: {
+                type: "string",
+                default: "aberto",
+                enum: ["aberto", "solucionado"],
+              },
+            },
           ],
           responses: {
             200: {
-              description: "Operação bem-sucedida",
-              content: {
-                "application/json": {
-                  schema: {
-                    $ref: "#/components/schemas/Agente",
-                  },
-                  examples: {
-                    Agente: {
-                      $ref: "#/components/examples/ExampleAgente",
-                    },
-                  },
+              $ref: "#/components/responses/CasoArray",
+            },
+          },
+        },
+        post: {
+          tags: ["casos"],
+          summary: "Cadastre um novo caso",
+          description: "Somente o atributo ID não deve ser enviado.",
+          requestBody: {
+            description: "Cria um novo caso",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/idless/Caso",
                 },
               },
             },
+            required: true,
+          },
+          responses: {
+            201: {
+              $ref: "#/components/responses/Caso",
+            },
+            400: {
+              $ref: "#/components/responses/MalformedData",
+            },
+          },
+        },
+      },
+      "/casos/{id}": {
+        get: {
+          tags: ["casos"],
+          summary: "Obtenha informações do caso pelo ID",
+          description:
+            "Retorna todas as informações referentes ao caso especificado pelo ID.",
+          produces: "application/json",
+          parameters: [{ $ref: "#/components/parameters/idParam" }],
+          responses: {
+            200: {
+              $ref: "#/components/responses/Caso",
+            },
+          },
+        },
+        put: {
+          tags: ["casos"],
+          summary: "Atualize todas as informações do caso",
+          description:
+            "Todos os parâmetros devem ser passados no corpo de requisição.",
+          parameters: [{ $ref: "#/components/parameters/idParam" }],
+          requestBody: {
+            description: "Atualiza informações do caso",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/idless/Caso",
+                },
+              },
+            },
+          },
+          responses: {
+            200: {
+              $ref: "#/components/responses/Caso",
+            },
+            400: {
+              $ref: "#/components/responses/MalformedData",
+            },
             404: {
               $ref: "#/components/responses/InvalidId",
+            },
+          },
+        },
+        patch: {
+          tags: ["casos"],
+          summary: "Atualize algumas informações do caso",
+          description:
+            "Deve ser passado no mínimo um atributo no corpo de requisição.",
+          parameters: [{ $ref: "#/components/parameters/idParam" }],
+          requestBody: {
+            description: "Atualiza informações do caso",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/idless/Caso",
+                },
+              },
+            },
+          },
+          responses: {
+            200: {
+              $ref: "#/components/responses/Caso",
+            },
+            400: {
+              $ref: "#/components/responses/MalformedData",
+            },
+            404: {
+              $ref: "#/components/responses/InvalidId",
+            },
+          },
+        },
+        delete: {
+          tags: ["casos"],
+          summary: "Apague um caso",
+          description: "Apague todas as informações de um caso.",
+          parameters: [{ $ref: "#/components/parameters/idParam" }],
+          responses: {
+            204: {
+              description: "O caso foi apagado",
+            },
+          },
+        },
+      },
+      "/casos/{caso_id}/agente": {
+        get: {
+          tags: ["casos"],
+          summary: "Obtenha informações do agente de um caso",
+          description:
+            "Retorna os dados completos do agente responsável por um caso específico.",
+          produces: "application/json",
+          parameters: [{ $ref: "#/components/parameters/caso_idParam" }],
+          responses: {
+            200: {
+              $ref: "#/components/responses/Agente",
+            },
+            404: {
+              $ref: "#/components/responses/InvalidId",
+            },
+          },
+        },
+      },
+      "/casos/search": {
+        get: {
+          tags: ["casos"],
+          summary: "Pesquisa de termos no título/descrição",
+          description:
+            "Retorna todos os casos em que a palavra da query string aparece no título e/ou descrição.",
+          produces: "application/json",
+          parameters: [
+            {
+              name: "q",
+              in: "query",
+              description: "Termos de pesquisa",
+              required: true,
+              schema: {
+                type: "string",
+              },
+            },
+          ],
+          responses: {
+            200: {
+              $ref: "#/components/responses/CasoArray",
             },
           },
         },
@@ -155,7 +383,6 @@ export const option = {
             },
             cargo: {
               type: "string",
-              enum: ["inspetor", "delegado"],
             },
           },
         },
@@ -175,6 +402,7 @@ export const option = {
             },
             status: {
               type: "string",
+              enum: ["aberto", "solucionado"],
             },
             agente_id: {
               type: "string",
@@ -207,6 +435,14 @@ export const option = {
       examples: {
         ExempleAgente: {
           value: {
+            id: "401bccf5-cf9e-489d-8412-446cd169a0f1",
+            nome: "Rommel Carneiro",
+            dataDeIncorporacao: "1992-10-04",
+            cargo: "delegado",
+          },
+        },
+        ExempleCaso: {
+          value: {
             id: "f5fb2ad5-22a8-4cb4-90f2-8733517a0d46",
             titulo: "homicidio",
             descricao:
@@ -215,13 +451,27 @@ export const option = {
             agente_id: "401bccf5-cf9e-489d-8412-446cd169a0f1",
           },
         },
-        ExempleCaso: {
-          value: {
-            id: "401bccf5-cf9e-489d-8412-446cd169a0f1",
-            nome: "Rommel Carneiro",
-            dataDeIncorporacao: "1992-10-04",
-            cargo: "delegado",
-          },
+        ExempleArrayAgente: {
+          value: [
+            {
+              id: "401bccf5-cf9e-489d-8412-446cd169a0f1",
+              nome: "Rommel Carneiro",
+              dataDeIncorporacao: "1992-10-04",
+              cargo: "delegado",
+            },
+          ],
+        },
+        ExempleArrayCaso: {
+          value: [
+            {
+              id: "f5fb2ad5-22a8-4cb4-90f2-8733517a0d46",
+              titulo: "homicidio",
+              descricao:
+                "Disparos foram reportados às 22:33 do dia 10/07/2007 na região do bairro União, resultando na morte da vítima, um homem de 45 anos.",
+              status: "aberto",
+              agente_id: "401bccf5-cf9e-489d-8412-446cd169a0f1",
+            },
+          ],
         },
         Error400: {
           value: {
@@ -279,6 +529,72 @@ export const option = {
             },
           },
         },
+        Agente: {
+          description: "Operação bem-sucedida",
+          content: {
+            "application/json": {
+              schema: {
+                $ref: "#/components/schemas/Agente",
+              },
+              examples: {
+                Agente: {
+                  $ref: "#/components/examples/ExempleAgente",
+                },
+              },
+            },
+          },
+        },
+        AgenteArray: {
+          description: "Operação bem-sucedida",
+          content: {
+            "application/json": {
+              schema: {
+                type: "array",
+                items: {
+                  $ref: "#/components/schemas/Agente",
+                },
+              },
+              examples: {
+                AgenteArray: {
+                  $ref: "#/components/examples/ExempleArrayAgente",
+                },
+              },
+            },
+          },
+        },
+        Caso: {
+          description: "Operação bem-sucedida",
+          content: {
+            "application/json": {
+              schema: {
+                $ref: "#/components/schemas/Caso",
+              },
+              examples: {
+                Caso: {
+                  $ref: "#/components/examples/ExempleCaso",
+                },
+              },
+            },
+          },
+        },
+        CasoArray: {
+          description: "Operação bem-sucedida",
+          content: {
+            "application/json": {
+              schema: {
+                type: "array",
+                items: {
+                  $ref: "#/components/schemas/Caso",
+                },
+              },
+              examples: {
+                CasoArray: {
+                  $ref: "#/components/examples/ExempleArrayCaso",
+                },
+              },
+            },
+          },
+        },
       },
       idless: {
         Agente: {
@@ -321,6 +637,28 @@ export const option = {
               format: "uuid",
               example: "401bccf5-cf9e-489d-8412-446cd169a0f1",
             },
+          },
+        },
+      },
+      parameters: {
+        idParam: {
+          name: "id",
+          in: "path",
+          description: "ID único do objeto",
+          required: true,
+          schema: {
+            type: "string",
+            format: "uuid",
+          },
+        },
+        caso_idParam: {
+          name: "caso_id",
+          in: "path",
+          description: "ID único do caso",
+          required: true,
+          schema: {
+            type: "string",
+            format: "uuid",
           },
         },
       },
